8b96fcd455b37591ef3454298f9517b8a82f7bfd
furkanese <mfurkaneseoglu@gmail.com>
2017-10-21 16:19:58 +0300

    bw and grey encoding

--- image_codec.py
+++ image_codec.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy
 
 
-def encode_image(pixels, rows, columns, encode_type):
+def encode_image_rowcol(pixels, rows, columns, encode_type):
     '''
     :param pixels: pixels of the image
     :param rows: row size
@@ -27,7 +27,7 @@ def encode_image(pixels, rows, columns, encode_type):
         # we are looping through the image
         if i % 2 == 0:
             for j in range(0, inner_index):
-                if not (i == 0) & (j == 0): # we do not control the first pixel of image
+                if not (i == 0) & (j == 0):  # we do not control the first pixel of image
                     if encode_type == 'row':
                         next_color = pixels[i][j]
                     else:
@@ -42,7 +42,7 @@ def encode_image(pixels, rows, columns, encode_type):
                         cntr = 1
         else:
             for j in range(inner_index - 1, -1, -1):
-                if not (i == 0) & (j ==0): # we do not control the first pixel of image
+                if not (i == 0) & (j == 0):  # we do not control the first pixel of image
                     if encode_type == 'row':
                         next_color = pixels[i][j]
                     else:
@@ -60,6 +60,78 @@ def encode_image(pixels, rows, columns, encode_type):
     colors.append(prev_color)
     return colors
 
+
+def encode_zigzag(pixels):
+    """
+    Encodes image zigzag-wise
+    :param pixels:
+    :return:
+    """
+    zigConverted = []
+    encoded_zigzag = []
+
+
+    # zigzag formation to array
+    i = j = bool = 0
+    zigConverted.append(pixels[i, j])
+    j = j + 1
+    while i < pixels.shape[0] and j < pixels.shape[1]:  # out of boundary gelirse = silinecek
+        if bool == 0:
+            while i >= 0 and pixels.shape[1] > j >= 0:
+                zigConverted.append(pixels[i, j])
+                j = j - 1
+                i = i + 1
+            j = 0
+            bool = 1
+        else:
+            while pixels.shape[0] > i >= 0 and j >= 0:
+                zigConverted.append(pixels[i, j])
+                j = j + 1
+                i = i - 1
+            i = 0
+            bool = 0
+    if bool == 0:
+        i = i + 1
+        j = j - 1
+    else:
+        j = j + 1
+        i = i - 1
+    bool = 0
+    while pixels.shape[0] > i >= 0 and pixels.shape[1] > j >= 0:
+        if bool == 0:
+            while i < pixels.shape[0] and j < pixels.shape[1]:  # out of boundary gelirse = silinecek
+                zigConverted.append(pixels[i, j])
+                j = j + 1
+                i = i - 1
+            j = pixels.shape[1] - 1
+            i = i + 2
+            bool = 1
+        else:
+            while i < pixels.shape[0] and j < pixels.shape[1]:
+                zigConverted.append(pixels[i, j])
+                j = j - 1
+                i = i + 1
+            i = pixels.shape[0] - 1
+            j = j + 2
+            bool = 0
+
+    # encoding
+    prev_color = 255  # first color taken as white
+    cntr = 1
+    for i in range (1,len(zigConverted)):
+        next_color = zigConverted[i]
+
+        if next_color == prev_color:
+            cntr += 1
+        else:
+            encoded_zigzag.append(cntr)
+            encoded_zigzag.append(prev_color)
+            prev_color = next_color
+            cntr = 1
+
+    # print(zigConverted)
+    # print(len(zigConverted))
+    return zigConverted, encoded_zigzag
 
 def squarify_image(pixels):
     """
--- image_comp.py
+++ image_comp.py
@@ -26,10 +26,10 @@ im_grayscale = cv2.imread('result_grayscale4.bmp',0)
 color_gray = cv2.split(im_grayscale)
 (g_channel,g_row, g_col) = np.shape(color_gray)
 
-# gray_row_encoded = image_codec.encode_image(color_gray[0], g_row, g_col, 'row')
+# gray_row_encoded = image_codec.encode_image_rowcol(color_gray[0], g_row, g_col, 'row')
 # np.savetxt('biber_gray_encode_row.txt', gray_row_encoded, fmt='%d',newline=' ')
 #
-# gray_col_encoded = image_codec.encode_image(color_gray[0], g_row, g_col, 'col')
+# gray_col_encoded = image_codec.encode_image_rowcol(color_gray[0], g_row, g_col, 'col')
 # np.savetxt('biber_gray_encode_col.txt', gray_col_encoded, fmt='%d',newline=' ')
 
 '''
@@ -40,12 +40,18 @@ im_bw = cv2.imread('res_bw.bmp',0)
 color_bw = cv2.split(im_bw)
 (bw_channel, bw_row, bw_col) = np.shape(color_bw)
 
-bw_row_encoded = image_codec.encode_image(color_bw[0], bw_row, bw_col, 'row')
-print(np.shape(bw_row_encoded))
-np.savetxt('biber_bw_encode_row.txt', bw_row_encoded, fmt='%d',newline=' ')
+# bw_row_encoded = image_codec.encode_image_rowcol(color_bw[0], bw_row, bw_col, 'row')
+# np.savetxt('biber_bw_encode_row.txt', bw_row_encoded, fmt='%d',newline=' ')
+#
+# bw_col_encoded = image_codec.encode_image_rowcol(color_bw[0], bw_row, bw_col, 'col')
+# np.savetxt('biber_bw_encode_col.txt', bw_col_encoded, fmt='%d',newline=' ')
 
-bw_col_encoded = image_codec.encode_image(color_bw[0], bw_row, bw_col, 'col')
-np.savetxt('biber_bw_encode_col.txt', bw_col_encoded, fmt='%d',newline=' ')
+bw_zig, bw_zig_encoded = image_codec.encode_zigzag(color_bw[0])
+np.savetxt('biber_bw_encode_zigzag.txt', bw_zig_encoded, fmt='%d',newline=' ')
+print(bw_zig)
+print(len(bw_zig))
+print(bw_zig_encoded)
+print(len(bw_zig_encoded))
 
 cv2.imshow('bw', im_bw)
 cv2.imshow('image', im_color)
