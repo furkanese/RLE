bb577c0e77df580f151feb57c93b4f5be61b64b4
furkanese <mfurkaneseoglu@gmail.com>
2017-10-22 14:07:15 +0300

    fixed uneven picture decoding

--- convert_image.py
+++ convert_image.py
@@ -6,7 +6,7 @@ import cv2
 CONVERSION BLOCK
 By Berk Taşkın
 '''
-col=Image.open('biber.bmp')
+col=Image.open('lenna.bmp')
 # converting to 7 bit grayscale and saving
 gray_scale = col.convert("L")
 gray_scale.save('result_grayscale7.bmp')
@@ -14,12 +14,6 @@ gray_scale.save('result_grayscale7.bmp')
 gray_scale4 = gray_scale.point(lambda x: int(x / 17) * 17)
 gray_scale4.save('result_grayscale4.bmp')
 # conversion and saving of BW image
-im_grayscale = cv2.imread('biber.bmp',0)
+im_grayscale = cv2.imread('lenna.bmp',0)
 (thresh, im_bw) = cv2.threshold(im_grayscale, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 cv2.imwrite('res_bw.bmp',im_bw)
-
-'''
-Pixel By Pixel encoding
-'''
-color_space= cv2.imread('biber.bmp')
-b,g,r = cv2.split(color_space)
--- image_comp_main.py
+++ image_comp_main.py
@@ -13,7 +13,7 @@ import image_codec
 from PIL import Image
 
 
-col=Image.open('goldhill.bmp')
+col=Image.open('ucak.bmp')
 # converting to 7 bit grayscale and saving
 gray_scale = col.convert("L")
 gray_scale.save('result_grayscale7.bmp')
@@ -21,12 +21,12 @@ gray_scale.save('result_grayscale7.bmp')
 gray_scale4 = gray_scale.point(lambda x: int(x / 17) * 17)
 gray_scale4.save('result_grayscale4.bmp')
 # conversion and saving of BW image
-im_grayscale = cv2.imread('goldhill.bmp',0)
+im_grayscale = cv2.imread('ucak.bmp',0)
 (thresh, im_bw) = cv2.threshold(im_grayscale, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 cv2.imwrite('res_bw.bmp',im_bw)
 
 
-im_color = cv2.imread('goldhill.bmp')
+im_color = cv2.imread('ucak.bmp')
 im_bw = cv2.imread('res_bw.bmp',0)
 im_grayscale = cv2.imread('result_grayscale4.bmp',0)
 encoded_shapes = []
@@ -42,54 +42,54 @@ original_shapes.append(rgb_row * rgb_col * 3)
 
 # ROW
 r_row_encoded = image_codec.encode_image_rowcol(color_r, rgb_row, rgb_col, 'row')
-np.savetxt('goldhill/goldhill_rgb_encode_r_row.txt', r_row_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_rgb_encode_r_row.txt', r_row_encoded, fmt='%d',newline=' ')
 g_row_encoded = image_codec.encode_image_rowcol(color_g, rgb_row, rgb_col, 'row')
-np.savetxt('goldhill/goldhill_rgb_encode_g_row.txt', g_row_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_rgb_encode_g_row.txt', g_row_encoded, fmt='%d',newline=' ')
 b_row_encoded = image_codec.encode_image_rowcol(color_b, rgb_row, rgb_col, 'row')
-np.savetxt('goldhill/goldhill_rgb_encode_b_row.txt', b_row_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_rgb_encode_b_row.txt', b_row_encoded, fmt='%d',newline=' ')
 
 encoded_shapes.append(len(b_row_encoded) + len(g_row_encoded) + len(r_row_encoded))
 
-r_row_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_rgb_encode_r_row.txt','row')
-g_row_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_rgb_encode_g_row.txt','row')
-b_row_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_rgb_encode_b_row.txt','row')
+r_row_decoded = image_codec.decode_image_rowcol('ucak/ucak_rgb_encode_r_row.txt','row')
+g_row_decoded = image_codec.decode_image_rowcol('ucak/ucak_rgb_encode_g_row.txt','row')
+b_row_decoded = image_codec.decode_image_rowcol('ucak/ucak_rgb_encode_b_row.txt','row')
 
-image_codec.save_as_image_rgb(r_row_decoded,g_row_decoded,b_row_decoded,'goldhill/goldhill_rgb_row.bmp')
+image_codec.save_as_image_rgb(r_row_decoded,g_row_decoded,b_row_decoded,'out/ucak_rgb_row.bmp')
 
 # COLUMN
 
 r_col_encoded = image_codec.encode_image_rowcol(color_r, rgb_row, rgb_col, 'col')
-np.savetxt('goldhill/goldhill_rgb_encode_r_col.txt', r_col_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_rgb_encode_r_col.txt', r_col_encoded, fmt='%d',newline=' ')
 g_col_encoded = image_codec.encode_image_rowcol(color_g, rgb_row, rgb_col, 'col')
-np.savetxt('goldhill/goldhill_rgb_encode_g_col.txt', g_col_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_rgb_encode_g_col.txt', g_col_encoded, fmt='%d',newline=' ')
 b_col_encoded = image_codec.encode_image_rowcol(color_b, rgb_row, rgb_col, 'col')
-np.savetxt('goldhill/goldhill_rgb_encode_b_col.txt', b_col_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_rgb_encode_b_col.txt', b_col_encoded, fmt='%d',newline=' ')
 
 encoded_shapes.append(len(b_col_encoded) + len(r_col_encoded) + len(g_col_encoded))
 
-r_col_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_rgb_encode_r_col.txt','col')
-g_col_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_rgb_encode_g_col.txt','col')
-b_col_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_rgb_encode_b_col.txt','col')
+r_col_decoded = image_codec.decode_image_rowcol('ucak/ucak_rgb_encode_r_col.txt','col')
+g_col_decoded = image_codec.decode_image_rowcol('ucak/ucak_rgb_encode_g_col.txt','col')
+b_col_decoded = image_codec.decode_image_rowcol('ucak/ucak_rgb_encode_b_col.txt','col')
 
-image_codec.save_as_image_rgb(r_col_decoded,g_col_decoded,b_col_decoded,'goldhill/goldhill_rgb_col.bmp')
+image_codec.save_as_image_rgb(r_col_decoded,g_col_decoded,b_col_decoded,'out/ucak_rgb_col.bmp')
 
 # ZIG ZAG
 
 r_zig, r_zig_encoded = image_codec.encode_zigzag(color_r)
-np.savetxt('goldhill/goldhill_r_encode_zigzag.txt', r_zig_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_r_encode_zigzag.txt', r_zig_encoded, fmt='%d',newline=' ')
 g_zig, g_zig_encoded = image_codec.encode_zigzag(color_g)
-np.savetxt('goldhill/goldhill_g_encode_zigzag.txt', g_zig_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_g_encode_zigzag.txt', g_zig_encoded, fmt='%d',newline=' ')
 b_zig, b_zig_encoded = image_codec.encode_zigzag(color_b)
-np.savetxt('goldhill/goldhill_b_encode_zigzag.txt', b_zig_encoded, fmt='%d',newline=' ')
+np.savetxt('ucak/ucak_b_encode_zigzag.txt', b_zig_encoded, fmt='%d',newline=' ')
 
 encoded_shapes.append(len(b_zig_encoded) + len(g_zig_encoded) + len(r_zig_encoded))
 
 
-r_zig_decoded = image_codec.decode_zigzag('goldhill/goldhill_r_encode_zigzag.txt')
-g_zig_decoded = image_codec.decode_zigzag('goldhill/goldhill_g_encode_zigzag.txt')
-b_zig_decoded = image_codec.decode_zigzag('goldhill/goldhill_b_encode_zigzag.txt')
+r_zig_decoded = image_codec.decode_zigzag('ucak/ucak_r_encode_zigzag.txt')
+g_zig_decoded = image_codec.decode_zigzag('ucak/ucak_g_encode_zigzag.txt')
+b_zig_decoded = image_codec.decode_zigzag('ucak/ucak_b_encode_zigzag.txt')
 
-image_codec.save_as_image_rgb(r_zig_decoded,g_zig_decoded,b_zig_decoded,'goldhill/goldhill_decoded_rgb_zigzag.bmp')
+image_codec.save_as_image_rgb(r_zig_decoded,g_zig_decoded,b_zig_decoded,'out/ucak_decoded_rgb_zigzag.bmp')
 
 '''
 GRAY 4 BIT
@@ -99,24 +99,24 @@ color_gray = cv2.split(im_grayscale)
 original_shapes.append(g_row * g_col * 1)
 
 gray_row_encoded = image_codec.encode_image_rowcol(color_gray[0], g_row, g_col, 'row')
-np.savetxt('goldhill/goldhill_gray_encode_row.txt', gray_row_encoded, fmt='%d',newline=' ')
-gray_row_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_gray_encode_row.txt','row')
-image_codec.save_as_image_gray(gray_row_decoded,'goldhill/goldhill_decoded_gray_row.bmp')
+np.savetxt('ucak/ucak_gray_encode_row.txt', gray_row_encoded, fmt='%d',newline=' ')
+gray_row_decoded = image_codec.decode_image_rowcol('ucak/ucak_gray_encode_row.txt','row')
+image_codec.save_as_image_gray(gray_row_decoded,'out/ucak_decoded_gray_row.bmp')
 
 encoded_shapes.append(len(gray_row_encoded))
 
 gray_col_encoded = image_codec.encode_image_rowcol(color_gray[0], g_row, g_col, 'col')
-np.savetxt('goldhill/goldhill_gray_encode_col.txt', gray_col_encoded, fmt='%d',newline=' ')
-bw_col_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_gray_encode_col.txt','col')
-image_codec.save_as_image_gray(bw_col_decoded,'goldhill/goldhill_decoded_gray_col.bmp')
+np.savetxt('ucak/ucak_gray_encode_col.txt', gray_col_encoded, fmt='%d',newline=' ')
+bw_col_decoded = image_codec.decode_image_rowcol('ucak/ucak_gray_encode_col.txt','col')
+image_codec.save_as_image_gray(bw_col_decoded,'out/ucak_decoded_gray_col.bmp')
 
 encoded_shapes.append(len(gray_col_encoded))
 
 
 gray_zig, gray_zig_encoded = image_codec.encode_zigzag(color_gray[0])
-np.savetxt('goldhill/goldhill_gray_encode_zigzag.txt', gray_zig_encoded, fmt='%d',newline=' ')
-gray_zig_decoded = image_codec.decode_zigzag('goldhill/goldhill_gray_encode_zigzag.txt')
-image_codec.save_as_image_gray(gray_zig_decoded,'goldhill/goldhill_decoded_gray_zigzag.bmp')
+np.savetxt('ucak/ucak_gray_encode_zigzag.txt', gray_zig_encoded, fmt='%d',newline=' ')
+gray_zig_decoded = image_codec.decode_zigzag('ucak/ucak_gray_encode_zigzag.txt')
+image_codec.save_as_image_gray(gray_zig_decoded,'out/ucak_decoded_gray_zigzag.bmp')
 
 encoded_shapes.append(len(gray_zig_encoded))
 
@@ -131,28 +131,28 @@ color_bw = cv2.split(im_bw)
 original_shapes.append(bw_row * bw_col * 1)
 
 bw_row_encoded = image_codec.encode_image_rowcol(color_bw[0], bw_row, bw_col, 'row')
-np.savetxt('goldhill/goldhill_bw_encode_row.txt', bw_row_encoded, fmt='%d',newline=' ')
-bw_row_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_bw_encode_row.txt','row')
-image_codec.save_as_image_gray(bw_row_decoded,'goldhill/goldhill_decoded_bw_row.bmp')
+np.savetxt('ucak/ucak_bw_encode_row.txt', bw_row_encoded, fmt='%d',newline=' ')
+bw_row_decoded = image_codec.decode_image_rowcol('ucak/ucak_bw_encode_row.txt','row')
+image_codec.save_as_image_gray(bw_row_decoded,'out/ucak_decoded_bw_row.bmp')
 
 encoded_shapes.append(len(bw_row_encoded))
 
 bw_col_encoded = image_codec.encode_image_rowcol(color_bw[0], bw_row, bw_col, 'col')
-np.savetxt('goldhill/goldhill_bw_encode_col.txt', bw_col_encoded, fmt='%d',newline=' ')
-bw_col_decoded = image_codec.decode_image_rowcol('goldhill/goldhill_bw_encode_col.txt','col')
-image_codec.save_as_image_gray(bw_col_decoded,'goldhill/goldhill_decoded_bw_col.bmp')
+np.savetxt('ucak/ucak_bw_encode_col.txt', bw_col_encoded, fmt='%d',newline=' ')
+bw_col_decoded = image_codec.decode_image_rowcol('ucak/ucak_bw_encode_col.txt','col')
+image_codec.save_as_image_gray(bw_col_decoded,'out/ucak_decoded_bw_col.bmp')
 
 encoded_shapes.append(len(bw_col_encoded))
 
 bw_zig, bw_zig_encoded = image_codec.encode_zigzag(color_bw[0])
-np.savetxt('goldhill/goldhill_bw_encode_zigzag.txt', bw_zig_encoded, fmt='%d',newline=' ')
-bw_zig_decoded = image_codec.decode_zigzag('goldhill/goldhill_bw_encode_zigzag.txt')
-image_codec.save_as_image_gray(bw_zig_decoded,'goldhill/goldhill_decoded_bw_zigzag.bmp')
+np.savetxt('ucak/ucak_bw_encode_zigzag.txt', bw_zig_encoded, fmt='%d',newline=' ')
+bw_zig_decoded = image_codec.decode_zigzag('ucak/ucak_bw_encode_zigzag.txt')
+image_codec.save_as_image_gray(bw_zig_decoded,'out/ucak_decoded_bw_zigzag.bmp')
 
 encoded_shapes.append(len(bw_zig_encoded))
 
-np.savetxt('goldhill/goldhill_org.txt', original_shapes, fmt='%d')
-np.savetxt('goldhill/goldhill_enc.txt', encoded_shapes, fmt='%d')
+np.savetxt('ucak/ucak_org.txt', original_shapes, fmt='%d')
+np.savetxt('ucak/ucak_enc.txt', encoded_shapes, fmt='%d')
 
 
 cv2.imshow('bw', im_bw)
